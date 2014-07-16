81d9581ef107bfefc1b4e0b8ea7d0ea612f8d3ab
Chiefree <banghh90@gmail.com>
2014-07-16 21:26:58 +0700

    testing with @dataProvider

--- phpUnitDemo/Test/URLTest.php
+++ phpUnitDemo/Test/URLTest.php
@@ -10,18 +10,29 @@ use phpUnitDemo\URL;
 class URLTest extends \PHPUnit_Framework_TestCase
 {
 	/**
-	 * This string will be sluggified” will turn into “this-string-will-be-sluggified
-	 * @return [type] [description]
+	 * [testSluggifyReturnsSluggifiedString description]
+	 * @param  string $originalString String to be sluggified
+	 * @param  string $expectedResult What we expect our slug result to be
+	 * @dataProvider providerTestSluggifyReturnsSluggifiedString
 	 */
-	public function testSluggifyReturnsSluggifiedString()
+	public function testSluggifyReturnsSluggifiedString($originalString, $expectedResult)
 	{
-		$originalString = "This string will be sluggified";
-		$expectedResult = "this-string-will-be-sluggified";
-
 		$url = new URL();
 
 		$result = $url->sluggify($originalString);
 
 		$this->assertEquals($result, $expectedResult);
+	}
+
+	public function providerTestSluggifyReturnsSluggifiedString()
+	{
+		return array(
+            array('This string will be sluggified', 'this-string-will-be-sluggified'),
+            array('THIS STRING WILL BE SLUGGIFIED', 'this-string-will-be-sluggified'),
+            array('This1 string2 will3 be 44 sluggified10', 'this1-string2-will3-be-44-sluggified10'),
+            array('This! @string#$ %$will ()be "sluggified', 'this-string-will-be-sluggified'),
+            array("Tänk efter nu – förr'n vi föser dig bort", 'tank-efter-nu-forrn-vi-foser-dig-bort'),
+            array('', ''),
+        );
 	}
 }
